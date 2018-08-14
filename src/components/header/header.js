7982ff4aafff677d515856d438f88e4558ea281c
wrqqq <wrqqqrrr@gmail.com>
2018-08-14 14:50:02 +0300

    Components structure, styles structure

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <div className="header-navigation">
+                <a href="tel:8(800) 500-75-55" className="header-navgiation-tel">8(800) 500-75-55</a>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
